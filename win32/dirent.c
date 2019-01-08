31f6f404c87a0fa1ae833fa138c51d168ffb37ff
Luke Usher <luke.usher@outlook.com>
2019-01-08 22:20:57 +0000

    Fix ISO creation on 64-bit Windows

--- win32/dirent.c
+++ win32/dirent.c
@@ -71,7 +71,7 @@ extern "C"
 
 struct DIR
 {
-    long                handle; /* -1 for failed rewind */
+    intptr_t            handle; /* -1 for failed rewind */
     struct _finddata_t  info;
     struct dirent       result; /* d_name null iff first time */
     char                *name;  /* null-terminated char string */
@@ -92,7 +92,7 @@ DIR *opendir(const char *name)
         {
             strcat(strcpy(dir->name, name), all);
 
-            if((dir->handle = (long) _findfirst(dir->name, &dir->info)) != -1)
+            if((dir->handle = _findfirst(dir->name, &dir->info)) != -1)
             {
                 dir->result.d_name = 0;
             }
@@ -166,7 +166,7 @@ void rewinddir(DIR *dir)
     if(dir && dir->handle != -1)
     {
         _findclose(dir->handle);
-        dir->handle = (long) _findfirst(dir->name, &dir->info);
+        dir->handle = _findfirst(dir->name, &dir->info);
         dir->result.d_name = 0;
     }
     else
