4348ca4545226e94429be98c3712e6086592d4fb
18woldemar <18woldemar@gmail.com>
2021-02-04 12:22:05 +0500

    Added redump-style images support

--- extract-xiso.c
+++ extract-xiso.c
@@ -3,6 +3,65 @@
 
 	An xdvdfs .iso (xbox iso) file extraction and creation tool by in <in@fishtank.com>
 		written March 10, 2003
+
+
+	View this file with your tab stops set to 4 spaces or it it will look wacky.
+
+
+	Regarding licensing:
+
+	I think the GPL sucks!  (it stands for Generosity Poor License)
+
+	My open-source code is really *FREE* so you can do whatever you want with it,
+	as long as 1) you don't claim that you wrote my code and 2) you retain a notice
+	that some parts of the code are copyright in@fishtank.com and 3) you understand
+	there are no warranties.  I only guarantee that it will take up disk space!
+
+	If you want to help out with this project it would be welcome, just email me at
+	in@fishtank.com.
+
+	This code is copyright in@fishtank.com and is licensed under a slightly modified
+	version of the Berkeley Software License, which follows:
+
+	/*
+	 * Copyright (c) 2003 in <in@fishtank.com>
+	 * All rights reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without
+	 * modification, are permitted provided that the following conditions
+	 * are met:
+	 *
+	 * 1. Redistributions of source code must retain the above copyright
+	 *    notice, this list of conditions and the following disclaimer.
+	 *
+	 * 2. Redistributions in binary form must reproduce the above copyright
+	 *    notice, this list of conditions and the following disclaimer in the
+	 *    documentation and/or other materials provided with the distribution.
+	 *
+	 * 3. All advertising materials mentioning features or use of this software
+	 *    must display the following acknowledgement:
+	 *
+	 *    This product includes software developed by in <in@fishtank.com>.
+	 *
+	 * 4. Neither the name "in" nor the email address "in@fishtank.com"
+	 *    may be used to endorse or promote products derived from this software
+	 *    without specific prior written permission.
+	 *
+	 * THIS SOFTWARE IS PROVIDED `AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES
+	 * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+	 * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+	 * AUTHOR OR ANY CONTRIBUTOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	 * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	 * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+	 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+	 * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+	 * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+	 * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 *\
+
+	enjoy!
+	
+	in
 */
 
 #if defined( __LINUX__ )
